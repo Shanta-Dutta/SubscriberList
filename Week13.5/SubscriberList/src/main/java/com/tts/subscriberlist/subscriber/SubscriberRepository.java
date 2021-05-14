8e253ef10b50e9b8d8cf167cc6e2669f7768e306
Shanta-Dutta <74420422+Shanta-Dutta@users.noreply.github.com>
2021-05-14 12:05:27 -0400

    Add files via upload

---
+++ Week13.5/SubscriberList/src/main/java/com/tts/subscriberlist/subscriber/SubscriberRepository.java
@@ -0,0 +1,8 @@
+package com.tts.subscriberlist.subscriber;
+import org.springframework.data.repository.CrudRepository;
+
+//import com.sun.tools.javac.comp.Subscriber;
+
+public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
+
+}
